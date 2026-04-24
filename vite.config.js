04ec7acf6aa7ff139f843ea38c3c8f8e19e90fac
Russ Frisch <russ.frisch@gmail.com>
2026-04-24 14:38:57 -0400

    Add index.html that loads compiled assets from dist/

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: './src/main.jsx',
     },
